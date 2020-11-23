5e142e78ee934346b828f32b3e74d437be84c360
Abstract_Factory <jovan.dimitrijevic@elfak.rs>
2020-11-23 20:47:26 +0100

    Bilo koja poruka

---
+++ App/RizzyCoBE/Migrations/20201123191157_FirstVersion.cs
@@ -0,0 +1,315 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RizzyCoBE.Migrations
+{
+    public partial class FirstVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Maps",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    NumberOfContinents = table.Column<int>(nullable: false),
+                    NumberOfTerritories = table.Column<int>(nullable: false),
+                    NumberOfAvailableArmies = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maps", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Missions",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(nullable: true),
+                    MapID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Missions", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Missions_Maps_MapID",
+                        column: x => x.MapID,
+                        principalTable: "Maps",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumberOfPlayers = table.Column<int>(nullable: false),
+                    Finished = table.Column<bool>(nullable: false),
+                    MapID = table.Column<int>(nullable: true),
+                    UserID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Games_Maps_MapID",
+                        column: x => x.MapID,
+                        principalTable: "Maps",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Games_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlayerColor",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(nullable: true),
+                    GameID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerColor", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PlayerColor_Games_GameID",
+                        column: x => x.GameID,
+                        principalTable: "Games",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GameID = table.Column<int>(nullable: true),
+                    MissionID = table.Column<int>(nullable: true),
+                    UserID = table.Column<int>(nullable: true),
+                    PlayerColorID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Players_Games_GameID",
+                        column: x => x.GameID,
+                        principalTable: "Games",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Players_Missions_MissionID",
+                        column: x => x.MissionID,
+                        principalTable: "Missions",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Players_PlayerColor_PlayerColorID",
+                        column: x => x.PlayerColorID,
+                        principalTable: "PlayerColor",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Players_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Territories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    PlayerID = table.Column<int>(nullable: true),
+                    MapID = table.Column<int>(nullable: true),
+                    TerritoryID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Territories", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Territories_Maps_MapID",
+                        column: x => x.MapID,
+                        principalTable: "Maps",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Territories_Players_PlayerID",
+                        column: x => x.PlayerID,
+                        principalTable: "Players",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Territories_Territories_TerritoryID",
+                        column: x => x.TerritoryID,
+                        principalTable: "Territories",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(nullable: true),
+                    Picture = table.Column<string>(nullable: true),
+                    PlayerID = table.Column<int>(nullable: true),
+                    TerritoryID = table.Column<int>(nullable: true),
+                    MapID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Cards_Maps_MapID",
+                        column: x => x.MapID,
+                        principalTable: "Maps",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Cards_Players_PlayerID",
+                        column: x => x.PlayerID,
+                        principalTable: "Players",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Cards_Territories_TerritoryID",
+                        column: x => x.TerritoryID,
+                        principalTable: "Territories",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_MapID",
+                table: "Cards",
+                column: "MapID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_PlayerID",
+                table: "Cards",
+                column: "PlayerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_TerritoryID",
+                table: "Cards",
+                column: "TerritoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_MapID",
+                table: "Games",
+                column: "MapID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_UserID",
+                table: "Games",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Missions_MapID",
+                table: "Missions",
+                column: "MapID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerColor_GameID",
+                table: "PlayerColor",
+                column: "GameID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_GameID",
+                table: "Players",
+                column: "GameID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_MissionID",
+                table: "Players",
+                column: "MissionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_PlayerColorID",
+                table: "Players",
+                column: "PlayerColorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_UserID",
+                table: "Players",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Territories_MapID",
+                table: "Territories",
+                column: "MapID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Territories_PlayerID",
+                table: "Territories",
+                column: "PlayerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Territories_TerritoryID",
+                table: "Territories",
+                column: "TerritoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "Territories");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Missions");
+
+            migrationBuilder.DropTable(
+                name: "PlayerColor");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Maps");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
