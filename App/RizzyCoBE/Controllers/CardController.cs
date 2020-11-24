f6ab385c232a542fe7d834187f6ce22230cc03a0
Abstract_Factory <jovan.dimitrijevic@elfak.rs>
2020-11-24 21:21:05 +0100

    local commit

---
+++ App/RizzyCoBE/Controllers/CardController.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RizzyCoBE.Models;
+
+namespace RizzyCoBE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardController : ControllerBase
+    {
+        private RizzyCoContext rizzyCoContext;
+
+        // Dependency Injection DB Context-a
+        public CardController(RizzyCoContext context)
+        {
+            rizzyCoContext = context;
+        }
+
+        [HttpGet]
+        [Route("GetCards")]
+        public async Task<JsonResult> GetCards()
+        {
+            var card = await rizzyCoContext.Cards
+                        .Include(c => c.Territory)
+                        .ToListAsync();
+
+            return new JsonResult(card);
+        }
+
+        [HttpPost]
+        [Route("AddCard")]
+        public async Task AddCard([FromBody] Card card)
+        {
+            rizzyCoContext.Cards.Add(card);
+            await rizzyCoContext.SaveChangesAsync();
+        }
+
+        // HTTP DELETE
+        [HttpDelete]
+        [Route("DeleteCard/{id}")]
+        // id se prosleđuje preko URL-a i upisuje u parametar
+        public async Task DeleteCard(int id)
+        {
+            var card = await rizzyCoContext.Cards.FindAsync(id);
+            rizzyCoContext.Cards.Remove(card);
+            await rizzyCoContext.SaveChangesAsync();
+        }
+
+        // HTTP PUT
+        [HttpPut]
+        [Route("ChangeCard/{id}/{newDescription}")]
+        public async Task ChangeCard(int id, string newPicture)
+        {
+            var card = await rizzyCoContext.Cards.FindAsync(id);
+            card.Picture = newPicture;
+            rizzyCoContext.Cards.Update(card);
+            await rizzyCoContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ App/RizzyCoBE/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RizzyCoBE.Models;
+
+namespace RizzyCoBE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private RizzyCoContext rizzyCoContext;
+
+        // Dependency Injection DB Context-a
+        public UserController(RizzyCoContext context)
+        {
+            rizzyCoContext = context;
+        }
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public async Task<JsonResult> GetUsers()
+        {
+            var user = await rizzyCoContext.Users
+                        .Include(u => u.Games)
+                        .ToListAsync();
+
+            return new JsonResult(user);
+        }
+
+        [HttpPost]
+        [Route("AddUser")]
+        public async Task AddUser([FromBody] User user)
+        {
+            rizzyCoContext.Users.Add(user);
+            await rizzyCoContext.SaveChangesAsync();
+        }
+
+        // HTTP DELETE
+        [HttpDelete]
+        [Route("DeleteUser/{id}")]
+        // id se prosleđuje preko URL-a i upisuje u parametar
+        public async Task DeleteUser(int id)
+        {
+            var user = await rizzyCoContext.Users.FindAsync(id);
+            rizzyCoContext.Users.Remove(user);
+            await rizzyCoContext.SaveChangesAsync();
+        }
+
+        // HTTP PUT
+        [HttpPut]
+        [Route("ChangeUser/{id}/{newUsername}")]
+        public async Task ChangeUser(int id, string newUsername)
+        {
+            var user = await rizzyCoContext.Users.FindAsync(id);
+            user.Username = newUsername;
+            rizzyCoContext.Users.Update(user);
+            await rizzyCoContext.SaveChangesAsync();
+        }
+    }
+}
